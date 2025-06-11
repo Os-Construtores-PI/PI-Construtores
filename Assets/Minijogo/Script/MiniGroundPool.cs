aa84905551eacbc18a73ad950d0ec2e6d0211b2b
PadeirinhoHiro <padeirinhohiro@gmail.com>
2025-06-11 19:30:21 -0300

    .

--- Assets/Minijogo/Script/MiniGroundPool.cs
+++
@@ -1,9 +0,0 @@
-public class MiniGroundPool : MiniPool
-{
-    public static MiniGroundPool _groundPool;
-    public override void Awake()
-    {
-        _groundPool = this;
-        base.Awake();
-    }
-}
