f9ffaf9ee451b70f7582f965e62318fd86b91778
Aclerson Rodrigues da Silva <padeirinhohiro@gmail.com>
2025-06-25 12:50:03 -0300

    ..

--- Assets/Script/Directors/GameDirector.cs
+++ Assets/Script/Directors/GameDirector.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class GameDirector : MonoBehaviour
 {
+    private DataSystem dataSystem;
+    private void Start()
+    {
+        if (!TryGetComponent(out dataSystem)) return;
+        dataSystem.Load();
+    }
+
     public void ShutdownWorld()
     {
 
