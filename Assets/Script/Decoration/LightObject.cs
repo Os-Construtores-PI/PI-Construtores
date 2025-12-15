a29c9d7c5970b969071a5ae7fb89966f110e01ab
PadeirinhoHiro <padeirinhohiro@gmail.com>
2025-12-15 20:50:04 -0300

    Ajustes

---
+++ Assets/Script/Decoration/LightObject.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LightObject : BaseRenderedGameObject
+{
+    [SerializeField] private float lightIntensity = 10;
+    public override void Start()
+    {
+        base.Start();
+        List<Light> lights = GetComponentsInChildren<Light>().ToList();
+        foreach(Light light in lights)
+        {
+            light.intensity = lightIntensity;
+        }
+    }
+
+}
