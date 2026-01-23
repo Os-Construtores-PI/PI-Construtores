e1f83a8c68e772c3949be44d349dbd8cf96df971
Leonardo Barros <leonardox445@gmail.com>
2026-01-23 20:43:26 -0300

    encorpando o tutorial

---
+++ Assets/Script/Tutorial/InputIconUI.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputIconUI : MonoBehaviour
+{
+    [SerializeField] private DeviceSpriteManager.InputIconType iconType;
+    [SerializeField] private Image targetImage;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    private void Awake()
+    {
+        if(targetImage == null)
+            targetImage = GetComponent<Image>();
+    }
+
+    private void OnEnable()
+    {
+        Atualizar(DeviceSpriteManager.Instance.GetCurrentDevice());
+
+        DeviceSpriteManager.Instance.OnDeviceChanged += Atualizar;
+    }
+
+    private void OnDisable()
+    {
+        if (DeviceSpriteManager.Instance != null)
+            DeviceSpriteManager.Instance.OnDeviceChanged -= Atualizar;
+    }
+
+    private void Atualizar(string device)
+    {
+        if (targetImage == null) return;
+
+        targetImage.sprite =
+            DeviceSpriteManager.Instance.GetSprite(iconType);
+    }
+}
