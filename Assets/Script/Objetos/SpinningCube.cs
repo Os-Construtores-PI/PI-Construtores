dd72f634857a961cbed0918d1da8330f7b19fe9e
Aclerson Silva <padeirinhohiro@gmail.com>
2026-03-04 19:58:40 -0300

    Ajustes e fim dos caminhos 50%

---
+++ Assets/Script/Objetos/SpinningCube.cs
@@ -0,0 +1,42 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class RotatingCube : BaseRenderedGameObject
+{
+  [SerializeField]
+  private float _xRotateSpeed;
+
+  [SerializeField]
+  private float _yRotateSpeed;
+
+  [SerializeField]
+  private float _zRotateSpeed;
+
+  [SerializeField]
+  private float _rotationDuration = 1;
+
+  [SerializeField]
+  private float _rotationInterval = 5;
+
+  public override void Start()
+  {
+    base.Start();
+    RotationAnimation();
+  }
+
+  private void RotationAnimation()
+  {
+    Vector3 rotationStep = new(_xRotateSpeed, _yRotateSpeed, _zRotateSpeed);
+
+    Sequence rotatingSequence = DOTween.Sequence();
+
+    rotatingSequence.Append(
+      transform
+        .DORotate(rotationStep, _rotationDuration, RotateMode.WorldAxisAdd)
+        .SetEase(Ease.Linear)
+    );
+
+    rotatingSequence.AppendInterval(_rotationInterval);
+    rotatingSequence.SetLoops(-1);
+  }
+}
