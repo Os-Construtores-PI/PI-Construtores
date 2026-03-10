2ff1472f25ca43ab7b001e54e71637d3b311bd4e
Aclerson Silva <padeirinhohiro@gmail.com>
2026-03-10 19:56:40 -0300

    Ajustes

--- Assets/Script/Componentes/Vida/HitboxComponent.cs
+++ Assets/Script/Componentes/Vida/HitboxComponent.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 // Componente que aplica dano a qualquer CombatEntities que entrar na área
 public class HitboxComponent : MonoBehaviour
 {
-    [Header("Parâmetros de Dano")]
-    [SerializeField] private float _maxDamage = 10f;       // Dano inicial
-    private float damage;
-    // Propriedade pública de acesso ao dano
-    [HideInInspector]
-    public float Damage
-    {
-        get => damage;
-        set => damage = value;
-    }
+  [Header("Parâmetros de Dano")]
+  [SerializeField]
+  private float _maxDamage = 10f; // Dano inicial
+  private float damage;
 
-    private void Start()
-    {
-        Damage = _maxDamage;
-    }
+  // Propriedade pública de acesso ao dano
+  [HideInInspector]
+  public float Damage
+  {
+    get => damage;
+    set => damage = value;
+  }
+
+  private void Start()
+  {
+    Damage = _maxDamage;
+  }
 }
