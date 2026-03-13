d350235d43735ae78858848b26d9dfdaad13c154
Aclerson Rodrigues da Silva <aclersonrodriguesdasilva@gmail.com>
2026-03-13 00:58:53 -0300

    Ajustes e Correções no Ódio

--- Assets/Script/Auxiliar/BasicMenuLogic.cs
+++ Assets/Script/Auxiliar/BasicMenuLogic.cs
@@ -3,39 +3,40 @@ using UnityEngine.SceneManagement;
 
 public class BasicMenuLogic : MonoBehaviour
 {
+  #region  === MENU GAMEOVER ===
+  public void Respawn()
+  {
+    GlobalEventBus.Instance.PLAYERTRIGGEREDRESPAWN.Invoke();
+  }
 
-    #region  === MENU GAMEOVER ===
-    public void Respawn()
-    {
-        GlobalEventBus.Instance.PLAYERTRIGGEREDRESPAWN.Invoke();
-    }
-    public void ResetScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-    #endregion === MENU GAMEOVER ===
+  public void ResetScene()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+  #endregion === MENU GAMEOVER ===
 
-    #region  === MENU PAUSE ===
-    public void OpenOptions()
-    {
-        GlobalEventBus.Instance.PLAYERTRIGGEREDOPTIONS.Invoke(true);
-    }
-    public void ContinueGame()
-    {
-        GlobalEventBus.Instance.PLAYERTRIGGEREDPAUSE.Invoke(false);
-    }
+  #region  === MENU PAUSE ===
+  public void OpenOptions()
+  {
+    GlobalEventBus.Instance.PLAYERTRIGGEREDOPTIONS.Invoke(true);
+  }
 
-    #endregion === MENU PAUSE ===
+  public void ContinueGame()
+  {
+    GlobalEventBus.Instance.PLAYERTRIGGEREDPAUSE.Invoke(false);
+  }
 
-    #region  === COMUNS ===
-    public void ExitToMainMenu()
-    {
-        Time.timeScale = 1f;
-        GlobalEventBus.Instance.PLAYERTRIGGEREDPAUSE.Invoke(false);
+  #endregion === MENU PAUSE ===
 
-        if(DataDirector.Instance != null)
-           DataDirector.Instance.ResetRunTimeState();
-        SceneManager.LoadScene(Constants.SceneNames.MainMenu);
-    }
-    #endregion
+  #region  === COMUNS ===
+  public void ExitToMainMenu()
+  {
+    Time.timeScale = 1f;
+    GlobalEventBus.Instance.PLAYERTRIGGEREDPAUSE.Invoke(false);
+
+    if (DataDirector.Instance != null)
+      DataDirector.Instance.ResetRunTimeState();
+    SceneManager.LoadScene(Constants.SceneNames.MainMenu);
+  }
+  #endregion
 }
