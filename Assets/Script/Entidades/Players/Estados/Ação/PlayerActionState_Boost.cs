d48c74ea2bfd7d4f321e9e7c520ed7ac19a22976
Aclerson Silva <padeirinhohiro@gmail.com>
2026-05-13 20:06:34 -0300

    Update PlayerActionState_Boost.cs

--- Assets/Script/Entidades/Players/Estados/Ação/PlayerActionState_Boost.cs
+++ Assets/Script/Entidades/Players/Estados/Ação/PlayerActionState_Boost.cs
@@ -9,9 +9,10 @@ public class PlayerActionStateBoost : IState<Player>
   public ActionType Type => ActionType.Boost;
   public HashSet<ActionType> IncompatibleActions => new();
 
-  private float _rotationSpeed = 60f;
+  private float _rotationSpeed = 30f;
   private float _boostUsage = 20;
   private float _velocity = 50f;
+  private float slopeLimit = 30f;
 
   public void Enter(Player player)
   {
@@ -24,7 +25,7 @@ public class PlayerActionStateBoost : IState<Player>
   public void Exit(Player player)
   {
     player.LocomotionLayer.ChangeState(player.GroundedS, player);
-    player.MovementVector += player.transform.forward * _velocity;
+    player.MovementVector += player.CharacterController.velocity;
     _velocity = 0f;
     player.MainCamera.Priority = 20;
     player.BoostCamera.Priority = 0;
@@ -37,16 +38,50 @@ public class PlayerActionStateBoost : IState<Player>
       player.MoveInput.x * _rotationSpeed * Time.deltaTime,
       Space.World
     );
-    Vector3 horizontal = player.transform.forward * _velocity;
+
+    Vector3 moveDir = OnSlope(player, out RaycastHit hit)
+      ? Vector3.ProjectOnPlane(player.transform.forward, hit.normal).normalized
+      : player.transform.forward;
+
+    Vector3 horizontal = moveDir * _velocity;
     player.MovementVector = new(horizontal.x, player.MovementVector.y, horizontal.z);
   }
 
   public void Update(Player player)
   {
     player.DashSlashBoostButton.Value -= _boostUsage * Time.deltaTime;
+
+    if (!player.IsGrounded)
+    {
+      player.ActionLayer.ExitState(this, player);
+      player.IsImpulsioned = true;
+    }
     if (player.DashSlashBoostButton.Value <= 0)
     {
       player.ActionLayer.ExitState(this, player);
     }
+  }
+
+  private bool OnSlope(Player player, out RaycastHit hit)
+  {
+    hit = default;
+
+    if (!player.IsGrounded)
+      return false;
+
+    if (
+      Physics.Raycast(
+        player.transform.position,
+        Vector3.down,
+        out hit,
+        player.CharacterController.height / 2 + 0.5f
+      )
+    )
+    {
+      float angle = Vector3.Angle(hit.normal, Vector3.up);
+      return angle > 0 && angle <= slopeLimit;
+    }
+
+    return false;
   }
 }
