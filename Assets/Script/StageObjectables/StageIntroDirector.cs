530106684a54433faa66715267277a2144c46ade
Leonardo Barros <leonardox445@gmail.com>
2026-09-08 19:15:52 -0300

    Revert "Reapply "Merge branch 'mobilePort' into refactor/EndPanel""
    This reverts commit 12d2a3e82104629e05553626a3dadfb552a4bf67.

--- Assets/Script/StageObjectables/StageIntroDirector.cs
+++ Assets/Script/StageObjectables/StageIntroDirector.cs
@@ -14,11 +14,6 @@ public class StageIntroDirector : MonoBehaviour
   [SerializeField]
   Image _stageNumber;
 
-  [Header("backGroundBlack")]
-  [SerializeField] GameObject _backGroundBlack;
-
-  [SerializeField] private float _backGroundHoldTime = 0.35f;
-
   [SerializeField]
   Image _stageTitle;
 
@@ -27,35 +22,18 @@ public class StageIntroDirector : MonoBehaviour
 
   public bool IsPlaying { get; private set; }
 
-
   public IEnumerator Play(StageIntroData data)
   {
     IsPlaying = true;
-
     Time.timeScale = 0;
 
-    // Garante que o fundo preto esteja ativo
-    if (_backGroundBlack != null)
-      _backGroundBlack.SetActive(true);
+    _root.SetActive(true);
 
-
-    // Configura os dados da fase
     _stageNumber.sprite = data.StageTitleSprite;
     _stageTitle.sprite = data.StageNumberSprite;
 
-    // Ativa a interface da intro
-    _root.SetActive(true);
-    
-
     foreach (var slide in slideObjects)
       slide.PlayEnterAnimation();
-
-    yield return new WaitForSecondsRealtime(_backGroundHoldTime);
-
-    if(_backGroundBlack != null)
-      _backGroundBlack.SetActive(false);
-
-
 
     yield return new WaitForSecondsRealtime(data.WaitTime);
 
@@ -70,9 +48,7 @@ public class StageIntroDirector : MonoBehaviour
     yield return new WaitForSecondsRealtime(0.15f);
 
     _root.SetActive(false);
-
     Time.timeScale = 1;
-
     IsPlaying = false;
   }
 }
