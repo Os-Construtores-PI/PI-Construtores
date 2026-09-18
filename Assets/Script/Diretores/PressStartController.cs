1f3f605a6ba92c72da6a82f5b7fce95649c95688
Aclerson Silva <padeirinhohiro@gmail.com>
2026-09-18 19:03:57 -0300

    Into Menu

--- Assets/Script/Diretores/PressStartController.cs
+++ Assets/Script/Diretores/PressStartController.cs
@@ -1,58 +1,88 @@
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PressStartController : MonoBehaviour
 {
-    [SerializeField] private LoadingScreen loadingScreen;
+  [SerializeField]
+  private LoadingScreen loadingScreen;
 
-    [SerializeField] private GameObject pressStartPanel;
+  [SerializeField]
+  private GameObject pressStartPanel;
 
-    [SerializeField] private string menuScene = "MainMenu";
+  [SerializeField]
+  private Image _logoNekora;
 
+  [SerializeField]
+  private Image _logoBreadStudios;
 
-    private bool stated;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  [SerializeField]
+  private List<Image> _imagesToFadeIn = new();
 
-    // Update is called once per frame
-    void Update()
+  [SerializeField]
+  private List<Image> _imagesToFadeOut = new();
+
+  [SerializeField]
+  private string menuScene = "MainMenu";
+
+  private bool stated;
+
+  void Start()
+  {
+    StartSequence();
+  }
+
+  private void StartSequence()
+  {
+    Sequence startSequence = DOTween.Sequence();
+    _imagesToFadeIn.ForEach(image => startSequence.Join(image.DOFade(1, .5f)));
+    startSequence.AppendInterval(.5f);
+    startSequence.Append(_logoNekora.DOFade(1, .7f));
+    startSequence.Append(_logoBreadStudios.DOFade(1, .7f));
+
+    startSequence.AppendInterval(1f);
+    _imagesToFadeOut.ForEach(image => startSequence.Join(image.DOFade(0, .5f)));
+    startSequence.AppendCallback(() =>
     {
-        if (!stated)
-        {
-            bool keyboard = 
-                Keyboard.current != null &&
-                Keyboard.current.anyKey.wasPressedThisFrame;
-            
-            bool gamepad =
-                Gamepad.current != null &&(
-                    Gamepad.current.startButton.wasPressedThisFrame ||
-                    Gamepad.current.startButton.wasPressedThisFrame
-                );
-            
-            if(keyboard || gamepad)
-            {
-                stated = true;
+      pressStartPanel.SetActive(true);
+    });
 
-                pressStartPanel.SetActive(false);
+    startSequence.Play();
+  }
 
-                loadingScreen.LoadScene(menuScene);
-            }
-
-        }
-        
-    }
-
-    public void PressStartButton()
+  void Update()
+  {
+    if (!stated)
     {
-        if (stated)
-            return;
+      bool keyboard = Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame;
 
+      bool gamepad =
+        Gamepad.current != null
+        && (
+          Gamepad.current.startButton.wasPressedThisFrame
+          || Gamepad.current.startButton.wasPressedThisFrame
+        );
+
+      if (keyboard || gamepad)
+      {
         stated = true;
-
         pressStartPanel.SetActive(false);
-
         loadingScreen.LoadScene(menuScene);
+      }
     }
+  }
 
-    
+  public void PressStartButton()
+  {
+    if (stated)
+      return;
+
+    stated = true;
+
+    pressStartPanel.SetActive(false);
+
+    loadingScreen.LoadScene(menuScene);
+  }
 }
